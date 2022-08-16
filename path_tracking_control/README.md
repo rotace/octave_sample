2c55c79771f983b2107efc884b3d9f7eec0fa0f5
Yasunori Shibata <rotace2012227ys@gmail.com>
2022-08-16 19:51:40 +0900

    add rear_wheel_feedback

--- path_tracking_control/README.md
+++ path_tracking_control/README.md
@@ -7,6 +7,8 @@
 | フォルダ | 内容 |
 | --- | --- |
 | pure_pursuit | 単純追跡アルゴリズム |
+| rear_wheel_feedback | Rear Wheel Feedback追跡アルゴリズム |
+| lqr | 線形二次レギュレータ追跡アルゴリズム |
 
 #### 参考文献
 
@@ -20,3 +22,16 @@
 #### 参考文献
 
 * [Pure pursuit アルゴリズムによる経路追従とPythonサンプルプログラム](https://myenigma.hatenablog.com/entry/2017/06/05/111007)
+
+## Rear Wheel Position Feedback アルゴリズム
+
+#### 参考文献
+
+* [Rear wheel position feedbackによる経路追従とPythonサンプルコード](https://myenigma.hatenablog.com/entry/2017/06/20/090853)
+
+
+## LQR アルゴリズム
+
+#### 参考文献
+
+* [線形二次レギュレータ(LQR)による経路追従制御とPythonサンプルプログラム](https://myenigma.hatenablog.com/entry/2017/08/08/153651)
---
+++ path_tracking_control/rear_wheel_feedback/main.m
@@ -0,0 +1,82 @@
+% ref: https://github.com/AtsushiSakai/PythonRobotics/blob/master/PathTracking/rear_wheel_feedback/rear_wheel_feedback.py
+function main
+    clear all
+
+    % Parameter
+    global Kp dt WB KTH KE
+    KTH = 1.0; % steering control paramter
+    KE = 0.5; % steering control paramter
+    Kp = 1.0; % speed proportional gain
+    dt = 0.1; % [s] time tick
+    WB = 2.9; % [m] wheel base vehicle
+    
+    % wave
+    cx = [0:0.5:50];
+    cy = sin(cx./5.0) .* cx ./2.0;
+
+    % spiral
+    tau = [0:0.02:5];
+    cx = 5*cos(tau).*tau/2;
+    cy = 5*sin(tau).*tau/2;
+
+    target_speed = 10.0 / 3.6; % [m/s]
+
+    T = 100.0; % max simulation time
+
+    % init
+    state = State(x=-0.0, y=-3.0, yaw=0.0, v=0.0);
+    target_course = TargetCourse(cx, cy);
+
+    last_idx = length(cx) - 1;
+    t = 0.0;
+    [tgt_idx, ~] = target_course.search_target_index(state);
+
+    hax1=subplot(1,1,1);
+    hold on
+    plot(hax1, cx, cy, "-");
+
+    h_tgt = plot(hax1, cx(tgt_idx), cy(tgt_idx), "rx");
+    while T >= t && last_idx > tgt_idx
+        % calc control input
+        ai = proportional_control(target_speed, state.v);
+        di = rear_wheel_feedback_steer_control(state, target_course);
+        state.update(ai, di);
+        target_course.disp();
+
+        plot(hax1, state.x, state.y, "b.")
+        set(h_tgt, "xdata", cx(tgt_idx));
+        set(h_tgt, "ydata", cy(tgt_idx));
+        pause(0.03);
+
+        t += dt;
+    end
+    
+end
+
+
+function a = proportional_control(target, current)
+    global k Kp
+    a = Kp * (target - current);
+end
+
+
+function delta = rear_wheel_feedback_steer_control(state, trajectory)
+    global WB KTH KE
+    [idx, e] = trajectory.search_target_index(state);
+    th = arg(state.calc_tangent_vector()/trajectory.calc_tangent_vector(idx)); % calc difference yaw
+    k = abs(trajectory.calc_normal_vector(idx)); % calc curvature
+    v = state.v;
+
+    omega = v * k * cos(th) / (1.0 - k * e);
+    omega = omega - KTH * abs(v) * th;
+    omega = omega - KE * v * sin(th) * e / th;
+
+    if th != 0.0 && omega != 0.0
+        delta = atan(WB * omega / v);
+    else
+        delta = 0.0
+    end
+
+    th
+    k
+end
